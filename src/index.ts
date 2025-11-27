2bcf5115f4f94c15e89b37a5beac5f041e3e09b3
Amirparsa Baghdadi <amirparsab9@gmail.com>
2025-11-27 13:42:57 +0330

    added html example

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,24 @@ app.get(
   }
 )
 
+app.get('/html', (c) => {
+  // This can also be achieved by importing an html file but it needs some rollup configs
+  return c.html(`
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h1>R1EC-Edge-Hono</h1>
+  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui, amet esse suscipit nobis labore velit modi similique, unde quis enim expedita possimus vel id commodi. Necessitatibus odio quam deleniti sapiente.</p>
+</body>
+</html>
+    `)
+})
+
 // Make it compatible with R1Cloud Edge
 export default {
   async fetch(request: Request) {
