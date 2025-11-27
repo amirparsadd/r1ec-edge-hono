d14dba0542224e8fffeea6ca3e44c061bca782cb
Amirparsa Baghdadi <amirparsab9@gmail.com>
2025-11-27 13:46:02 +0330

    added configurable port and hostname for dev mode

--- src/dev.ts
+++ src/dev.ts
@@ -1,10 +1,12 @@
+import "dotenv/config"
+
 import { serve } from "@hono/node-server"
 import { app } from "./index.js"
 
 serve({
   fetch: app.fetch,
-  port: 3000,
-  hostname: "0.0.0.0"
+  port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
+  hostname: process.env.HOSTNAME ? process.env.HOSTNAME : "0.0.0.0"
 }, (info) => {
   console.log(`App is running in dev mode at http://${info.address}:${info.port}`)
 })
